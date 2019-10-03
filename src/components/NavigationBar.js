1e89db11d95a746ed841d83ac24a418912f510fe
Shmoji <jackj_skate@hotmail.com>
2019-10-02 22:36:24 -0400

    Icons on sidebar are a lil better now

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap'; // Notice not reactstrap
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -38,8 +38,8 @@ export const NavigationBar = () => (
             </Form>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link><Link to="/">Home</Link></Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link><Link to="/about">About</Link></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,29 +7,34 @@ const MySideNav = styled(SideNav)`
     background-color: #222;
 `;
 
+/* Inline CSS */
+const IconStyle = {
+    fontSize: '1.75em',
+}
+
 export const Sidebar = () => (
     <MySideNav
         onSelect={(selected) => {
             
         }}
     >
-                <SideNav.Nav defaultSelected="home">
-                    <NavItem eventKey="home">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Home
-                        </NavText>
-                    </NavItem>
-                    <NavItem eventKey="devices">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Devices
-                        </NavText>
-                    </NavItem>
-                </SideNav.Nav>
+        <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+                <NavIcon>
+                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                </NavIcon>
+                <NavText>
+                    Home
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="clock">
+                <NavIcon>
+                    <i className="far fa-clock" style={ IconStyle }></i>
+                </NavIcon>
+                <NavText>
+                    Devices
+                </NavText>
+            </NavItem>
+        </SideNav.Nav>
     </MySideNav>
 )
