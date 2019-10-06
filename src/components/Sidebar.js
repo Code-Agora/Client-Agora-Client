2be2b4f08fcbcae056276fcd800342701b4570e0
Shandon Anderson <andersonshandon@gmail.com>
2019-10-05 22:22:31 -0400

    Added a fix for sidnav routing

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import styled from 'styled-components';
+import React from "react";
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
+import styled from "styled-components";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Home } from "../Home";
 
 const MySideNav = styled(SideNav)`
-    top: 56px;
-    background-color: #222;
+  top: 56px;
+  background-color: #222;
 `;
 
 /* Inline CSS */
 const IconStyle = {
-    fontSize: '1.75em',
-}
+  fontSize: "1.75em"
+};
 
 export const Sidebar = () => (
-    <MySideNav
-        onSelect={(selected) => {
-            
-        }}
-    >
-        <SideNav.Nav defaultSelected="home">
-            <NavItem eventKey="home">
-                <NavIcon>
-                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                </NavIcon>
-                <NavText>
-                    Home
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="clock">
-                <NavIcon>
-                    <i className="far fa-clock" style={ IconStyle }></i>
-                </NavIcon>
-                <NavText>
-                    Devices
-                </NavText>
-            </NavItem>
-        </SideNav.Nav>
+  <Router>
+    <MySideNav onSelect={selected => {}}>
+      <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="">
+          {/*I made this eventKey blank because it is required, but not used*/}
+          <NavIcon>
+            <Link to="/">
+              {/* This Link refernces the router below */}
+              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
+            </Link>
+          </NavIcon>
+        </NavItem>
+        <NavItem eventKey="clock">
+          <NavIcon>
+            <i className="far fa-clock" style={IconStyle}></i>
+          </NavIcon>
+          <NavText>Devices</NavText>
+        </NavItem>
+      </SideNav.Nav>
     </MySideNav>
-)
+
+    {/* Routes */}
+    <Route exact path="/" component={Home} />
+  </Router>
+);
